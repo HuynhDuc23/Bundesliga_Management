4b359135925aee7d8a5c2c74c6de24f63d305ae9
vanhau <maivanhau0307@gmail.com>
2024-06-10 16:47:29 +0700

    last update 1.0

--- backend/routers/match.router.js
+++ backend/routers/match.router.js
@@ -8,11 +8,9 @@ import {
   updateMatch,
   deleteMatch,
 } from "../controllers/Match.controllers.js";
-// import { verifyToken } from "../middlewares/Permission.middlewares.js";
 
 const matchRouter = Router();
 
-// matchRouter.get("/:id",getTeamInSeasonById);
 matchRouter.get("/",getAllMatches);
 matchRouter.get("/rank",rank);
 matchRouter.get("/rank/:id",getRanking);
--- backend/routers/match_Team.router.js
+++ backend/routers/match_Team.router.js
@@ -4,10 +4,8 @@ import {
   getMatchTeamByIdTeam,
   createMatchTeam,
   getMatchTeamsByMatchId
-//   updateMatch,
-//   deleteMatch,
+
 } from "../controllers/Match_Team.controllers.js";
-// import { verifyToken } from "../middlewares/Permission.middlewares.js";
 
 const matchTeamRouter = Router();
 
@@ -15,7 +13,5 @@ matchTeamRouter.get("/",getAllMatchTeams);
 matchTeamRouter.get("/view/idmatch/:id", getMatchTeamsByMatchId);
 matchTeamRouter.get("/:id", getMatchTeamByIdTeam);
 matchTeamRouter.post("/add-match", createMatchTeam);
-// matchTeamRouter.put("/:id", updateMatch);
-// matchTeamRouter.delete("/:id", deleteMatch);
 
 export default matchTeamRouter;
